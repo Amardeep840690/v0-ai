87248d70145c949b35928eff5dbb2e947605a108
Amardeep840690 <amar840690@gmail.com>
2026-07-21 07:54:46 +0530

    feat: add UI components and hooks
    - Introduced Skeleton component for loading placeholders.
    - Added Slider component with customizable range and thumb elements.
    - Implemented Toaster component for notifications with customizable icons and themes.
    - Created Spinner component for loading indicators.
    - Developed Switch component for toggle functionality.
    - Added Table component with subcomponents for structured data display.
    - Implemented Tabs component with triggers and content for tabbed navigation.
    - Created Textarea component for multi-line text input.
    - Developed ToggleGroup and Toggle components for grouped toggle functionality.
    - Added Tooltip component for contextual hints and information.
    - Implemented useIsMobile hook to detect mobile device view.
    - Added utility function for class name merging.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  IBM_Plex_Sans,
+  Source_Sans_3,
+} from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "../components/providers/theme-provider";
+
+const sourceSans3Heading = Source_Sans_3({
+  subsets: ["latin"],
+  variable: "--font-heading",
+});
+
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +42,27 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        ibmPlexSans.variable,
+        sourceSans3Heading.variable,
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
